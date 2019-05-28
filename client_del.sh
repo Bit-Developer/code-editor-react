b9e002f266923905c69efad273918ec677fcc887
Zhuang, Rong <jojozhuang@gmail.com>
2019-05-28 08:38:40 -0700

    useless del

--- client_del.sh
+++ client_del.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo -e "\nReady to delete procfile \n"
-echo "DEPLOY_TIME" + $DEPLOY_TIME
 echo "TRAVIS_BUILD_DIR" + $TRAVIS_BUILD_DIR
 #CURR_DIR="/home/travis/build/jojozhuang/code-editor-react"
 CURR_DIR=$TRAVIS_BUILD_DIR
